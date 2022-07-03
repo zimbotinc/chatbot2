a191ba8c243ad71bf220c39d52d02f89bdf305d9
zimbotinc <106673922+zimbotinc@users.noreply.github.com>
2022-07-03 21:22:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['27634090203'] 
-global.pemilik = ['27634090203'] //pemilik nomor
-global.premium = ['27634090203'] //premium number
+global.owner = ['918188019676'] 
+global.pemilik = ['918188019676'] //pemilik nomor
+global.premium = ['918188018676'] //premium number
 global.pengguna = 'Drips' //pengguna
 global.botnma = 'ZIM BOT INC' //botnama,botname
 global.ownernma = 'Drips' //ownernama,ownername
